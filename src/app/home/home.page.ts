6aeb5955db1e40949fbf2fbd6ed5bee64d54891a
hafizhvergiansyah <piagembul5555@gmail.com>
2024-09-30 19:48:43 +0700

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,7 +15,8 @@ export class HomePage {
   onOnInit() {}
 
   ionViewDidEnter() {
-    this.map = L.map('mapId').setView([-7.5662, 110.8275], 14);
+    // Mengatur tampilan peta pada posisi UGM
+    this.map = L.map('mapId').setView([-7.7744, 110.3740], 14);
 
     // Menambahkan TileLayer
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -23,51 +24,21 @@ export class HomePage {
     }).addTo(this.map);
 
     const customIcon = L.icon({
-      iconUrl: 'https://www.freepnglogos.com/uploads/lokasi-logo-png/lokasi-logo-house-location-marker-icon-transparent-png-svg-vector-24.png', 
-      iconSize: [80, 80], 
+      iconUrl: 'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/map-marker-1024.png',
+      iconSize: [80, 80],
       iconAnchor: [12, 41],
       popupAnchor: [1, -34]
     });
 
-    // Marker di Keraton Surakarta
-    const markerKeraton = L.marker([-7.5662, 110.8275], { icon: customIcon }).addTo(this.map);
-    const imageKeraton = 'https://th.bing.com/th/id/OIP.AyQ1MfbDtcbEw9nefde9qgHaD8?rs=1&pid=ImgDetMain';
+    // Marker di Universitas Gadjah Mada
+    const markerUGM = L.marker([-7.7744, 110.3740], { icon: customIcon }).addTo(this.map);
+    const imageUGM = 'https://ugm.ac.id/wp-content/uploads/2023/04/About-Hero.jpg'; // Sesuaikan URL gambar UGM
 
-    markerKeraton.bindPopup(`
-      <b>Keraton Surakarta</b><br>
-      Pusat kebudayaan Jawa dan tempat tinggal Sultan Surakarta.<br>
-      <img src="${imageKeraton}" alt="Keraton Surakarta" style="max-width: 100%; height: auto;"/>
+    markerUGM.bindPopup(`
+      <b>Universitas Gadjah Mada</b><br>
+      Salah satu universitas tertua dan terbesar di Indonesia yang terletak di Yogyakarta.<br>
+      <img src="${imageUGM}" alt="Universitas Gadjah Mada" style="max-width: 100%; height: auto;"/>
     `).openPopup();
-
-    // Marker di UNS
-    const markerUNS = L.marker([-7.558, 110.856], { icon: customIcon }).addTo(this.map);
-    const imageUNS = 'https://uns.ac.id/id/wp-content/uploads/2021/04/uns-masuk-peringkat-201-300-dunia.jpeg';
-
-    markerUNS.bindPopup(`
-      <b>Universitas Sebelas Maret (UNS)</b><br>
-      Salah satu universitas terkemuka di Indonesia.<br>
-      <img src="${imageUNS}" alt="UNS" style="max-width: 100%; height: auto;"/>
-    `);
-
-    // Marker di Pasar Legi
-    const markerPasar = L.marker([-7.562723450414307, 110.82424399421002], { icon: customIcon }).addTo(this.map);
-    const imagePasar = 'https://awsimages.detik.net.id/community/media/visual/2021/12/04/pasar-legi-solo_169.jpeg?w=700&q=90';
-
-    markerPasar.bindPopup(`
-      <b>Pasar Legi Solo</b><br>
-      Pasar tradisional yang populer di Surakarta.<br>
-      <img src="${imagePasar}" alt="Pura Mangkunegaran" style="max-width: 100%; height: auto;"/>
-    `);
-
-    // Marker di UMS
-    const markerUMS = L.marker([-7.5580102187851095, 110.77167260995654], { icon: customIcon }).addTo(this.map);
-    const imageUMS = 'https://1.bp.blogspot.com/-I3fehN-OPmQ/WNTJ0rVqfHI/AAAAAAAABzk/3rXjxuryaa8SdvQ_Lu2FXbclh7NJN1HuACLcB/s1600/UMS.jpg';
-
-    markerUMS.bindPopup(`
-      <b>Universitas Muhammadiyah Surakarta (UMS)</b><br>
-      Universitas swasta terkenal di Surakarta.<br>
-      <img src="${imageUMS}" alt="UMS" style="max-width: 100%; height: auto;"/>
-    `);
 
     // Base maps
     const satelliteLayer = L.tileLayer('https://{s}.google.com/vt?x={x}&y={y}&z={z}&s=Ga', {
@@ -88,6 +59,23 @@ export class HomePage {
       attribution: '&copy; <a href="https://carto.com/attributions">CARTO</a>'
     });
 
+    // Base maps tambahan
+    const esriWorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
+
+    const stamenWatercolor = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
+      attribution: '&copy; <a href="http://maps.stamen.com/">Stamen Maps</a>'
+    });
+
+    const esriTopoLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
+    });
+
+    const cartoLightLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://carto.com/attributions">CARTO</a>'
+    });
+
     // Menambahkan layer satellite sebagai base map awal
     satelliteLayer.addTo(this.map);
 
@@ -96,7 +84,11 @@ export class HomePage {
       'Satellite': satelliteLayer,
       'Topographic': topoLayer,
       'OpenStreetMap': osmLayer,
-      'CartoDB Dark Matter': cartoDBLayer
+      'CartoDB Dark Matter': cartoDBLayer,
+      'Esri World Imagery': esriWorldImagery,
+      'Stamen Watercolor': stamenWatercolor,
+      'Esri Topographic': esriTopoLayer,
+      'CartoDB Light': cartoLightLayer
     };
 
     L.control.layers(baseMaps).addTo(this.map);
